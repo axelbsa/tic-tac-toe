2ce83dcf450501c2f9afa90b651975ff3a017732
Axel Sanner <axelsanner@gmail.com>
2017-02-27 00:48:19 +0100

    chaning to jquery format

--- tic.js
+++ tic.js
@@ -31,15 +31,88 @@ function check_mate() {
         }
     }
 
+    for (i = 0; i < 9; i++)
+    {
+        var file = i / 3;
+        var rank = i % 3;
+
+        // Check all rows
+        for (j = file; j < j + 3; j++ )
+        {
+        
+        }
+
+    }
+    
+    // Very naive!!
+    /*
+    for (i = 0; i < 9; i++)
+    {
+
+        if (i === 0)
+        {
+            var 0_pot_mate = true;
+            for (i = 0; i < 3; i++)
+            {
+                if (i !== 0)
+                {
+                    0_pot_mate = false;
+                }
+            }
+        }
+        else if (i === 1)
+        {
+            for (i = 0; i < 9; i++)
+            {
+            }
+        }
+        else if (i === 2)
+        {
+            for (i = 0; i < 9; i++)
+            {
+            }
+        }
+    }
+    */
+
     console.log(x_vals);
     console.log(o_vals);
 
     return false;
 }
 
-function render(row) {
+$(".box").click(function() {
+    var _id = this.id
     var cross = '<span class="icon-cross"></span>';
     var circle = '<span class="icon-radio-unchecked"></span>';
+
+    if (this.children.length > 0 || mate === true)
+    {
+        console.log('Its empty');
+        return;
+    }
+    
+    if (~current_player%2)
+    {
+        // X turn
+        $(this).append(cross);
+    }
+    else
+    {
+        // O turn
+        $(this).append(circle);
+    }
+
+    // Check for mate
+    // mate = check_mate();
+    current_player++;
+
+    console.log(this.children);
+
+    return false;
+})
+
+function render(row) {
 
     var row_id = $('#' + row);
     //console.log(row_id.html());
