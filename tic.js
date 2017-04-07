b9c967363d1ea773b137ab2309ace57eba94549c
Axel Sanner <axel@xeneta.com>
2017-04-07 20:18:21 +0200

    Board class

--- tic.js
+++ tic.js
@@ -22,7 +22,8 @@ function create_children(current_board):
 var current_player = 0;
 var mate = false;
 var maxsize = 100000
-var choices = 5;
+var max_depth = 3;
+var total = 0;
 
 mate_index = [
     /* Rows */
@@ -40,41 +41,118 @@ mate_index = [
     [2, 4, 6]
 ];
 
+class Board {
 
-function get_open_squares() {
-    var open_squares = [];
-    var current_board = get_board();
+    constructor() {
+        this.x_vals = [];
+        this.o_vals = [];
+        this.board = new Array(9);
+        this.mate = false;
+        this.zero_board();
+    }
 
-    for (var i = 0; i < current_board.length; i++ ) {
-        if (current_board[i] == 0)
-            open_squares.push()
+    open_positions()
+    {
+        let _map = [];
+        for (let i = 0; i < this.x_vals.length; i++) 
+        {
+            if ((this.x_vals | this.o_vals) == 0)
+            {
+                _map.push(i);
+            }
+        }
+        return _map;
+    }
+
+    zero_board()
+    {
+        for (var i = 0; i < this.board.length; i++ )
+        {
+            this.board[i] = 0;
+        }
+    }
+
+    get_board_from_dom() 
+    {
+        let board = get_board();
+        this.x_vals = board.x_vals;
+        this.o_vals = board.o_vals;
+    }
+
+    set_new_position(player, position)
+    {
+        this.board[position] = player;
+
+        if (player == 'X'){
+            this.x_vals[position] = 1
+        }
+        if (player == 'O') {
+            this.o_vals[position] = 1
+        }
+
+    }
+
+    print_board()
+    {
+        for (let i = 0; i < this.board.length; i++)
+        {
+            if (i % 3 == 0) {
+                console.log();
+            }
+            console.log(this.board[i]);
+        }
+    }
+
+    check_for_mate() 
+    {
+        this.mate = check_mate(this.o_vals, this.x_vals);
+        return this.mate
     }
 }
 
 
 class Node {
 
-    constructor (d, value) {
-        this.value = value;
+    constructor(current_depth, board) {
+        this.value = null;
         this.children = [];
-        this.create_children(d);
+        this.create_children_2(current_depth, board);
     }
 
-    create_children_2(current_board) {
-        index_values = get_open_squares(current_board);
-        check_mate = false;
+    create_children_2(current_depth, current_board) {
 
-        for (var i = 0; i < index_values.length; i++) {
+        //console.log(current_board);
+
+        if (current_depth < 0) {
+            return;
+        }
+        // XXX useless, better baord handling
+        //var index_values = get_open_squares(current_board);
+        //second iteration fails in move_gen becuase mangled board data
+
+        // Made up vars
+        let check_mate = false;
+        let bot_winner = true;
+        let huma_winner = false;
+
+        let open_pos = current_board.open_positions();
+        console.log(open_pos);
+
+        for (var i = 0; i < open_pos.length; i++) {
             if (check_mate = true)
                 if (bot_winner = true)
-                    node.value = 1000000
+                    this.value = 1000000
                 if (huma_winner = true)
-                    node.value = -1000000
-                return
+                    this.value = -1000000
+                //return
 
-            make_move(index_values[i]);
-            new_board = place_value_on_board();
-            create_children(new_board)
+            //make_move(index_values[i]); // Just set the value in the array
+            let player = ~current_player%2 ? 'X' : 'O';
+            current_board.set_new_position(player, open_pos[i]);
+            console.log("Making babies: " + current_depth);
+            let n = new Node(current_depth-1, current_board);
+            this.children.push(n);
+            total += 1
         }
     }
 
@@ -94,6 +172,73 @@ class Node {
     }
 }
 
+
+function get_open_squares(current_board) {
+    var open_squares = [];
+
+    for (var i = 0; i < current_board.length; i++ ) {
+        if (current_board[i] == 0)
+            open_squares.push(i)
+    }
+
+    return open_squares
+}
+
+function move_generator(values=false) {
+
+    if (!values) {
+        var values = get_board();
+    }
+
+    // Finds all the moves the AI can do
+    let board  = new Array();
+
+    for (let i = 0; i < values['o_vals'].length; i++) 
+    {
+        x_val = values['x_vals'][i];
+        o_val = values['o_vals'][i];
+        if (!(x_val | o_val)) {
+            board.push(i);
+        }
+    }
+
+    return board;
+}
+
+
+function get_board() {
+    var x_vals = new Array(9);
+    var o_vals = new Array(9);
+
+    for (i = 0; i < 9; i++) 
+    {
+        x_vals[i] = 0;
+        o_vals[i] = 0;
+    }
+
+    // Create array of stupid html shit
+    for (let i = 0; i < 9; i++) 
+    {
+        let value = $('#' + i);
+        if (value.html())
+        {   
+            if (value.html().search(/icon-cross/) != -1)
+            {
+                x_vals[i] = 1;
+            }
+            else
+            {
+                o_vals[i] = 1;
+            }
+        }
+    }
+
+    return {
+        'check_mate': check_mate(o_vals, x_vals),
+        'o_vals': o_vals,
+        'x_vals': x_vals
+    };
+}
 
 
 // int alphabeta(Node node, int depth, int alpha, int beta, bool maximizing_player)
@@ -137,59 +282,6 @@ function alphabeta(node, depth, alpha, beta, maximizing_player)
         }
         return v;
     }
-}
-
-
-function move_generator() {
-
-    // Finds all the moves the AI can do
-    let values = get_board();
-    let board  = new Array(9);
-
-    for (let i = 0; i < values['o_vals'].length; i++) 
-    {
-        x_val = values['x_vals'][i];
-        o_val = values['o_vals'][i];
-        board[i] = x_val | o_val;
-    }
-
-    console.log(board);
-
-}
-
-
-function get_board() {
-    var x_vals = new Array(9);
-    var o_vals = new Array(9);
-
-    for (i = 0; i < 9; i++) 
-    {
-        x_vals[i] = 0;
-        o_vals[i] = 0;
-    }
-
-    // Create array of stupid html shit
-    for (let i = 0; i < 9; i++) 
-    {
-        let value = $('#' + i);
-        if (value.html())
-        {   
-            if (value.html().search(/icon-cross/) != -1)
-            {
-                x_vals[i] = 1;
-            }
-            else
-            {
-                o_vals[i] = 1;
-            }
-        }
-    }
-
-    return {
-        'check_mate': check_mate(o_vals, x_vals),
-        'o_vals': o_vals,
-        'x_vals': x_vals
-    };
 }
 
 
@@ -261,13 +353,29 @@ $(".box").click(function() {
     }
 
     // Check for mate
-    mate = get_board().check_mate;
-    move_generator();
+    var board = new Board();
+    board.get_board_from_dom();
+    console.log({"Board_class": board});
+
+    mate = board.check_for_mate();
+    console.log({"current_board" : get_board()});
+    
+    var move_tree = [];
+    moves = move_generator();
+    console.log({"move generator" : moves});
+    
+    for (var i = 0; i < moves.length; i++) { // Generate tree
+        var n = new Node(max_depth, board);
+        move_tree.push(n);
+    }
+
+    console.log(total);
+
+    //for (var i = 0; i < move_tree.length; i++) { // Run alphabeta
+        //console.log({
+            //"Value alpha beta":
+            //alphabeta(move_tree[i], 5, -maxsize, maxsize, true)
+        //});
+    //}
     current_player++;
-
-    // Test Node init
-    var fd = new Node(1);
-    console.log(alphabeta(fd, 5, -maxsize, maxsize, true));
-
-    console.log(fd);
 });
